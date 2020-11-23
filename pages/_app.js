be0025b91bbc5e615ae94d56d8a186522acbdfb6
fcrezza <fachrezanang@gmail.com>
2020-11-23 21:55:37 +0700

    setup project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import 'normalize.css';
+import 'fontsource-inter/400-normal.css';
+import 'fontsource-inter/500-normal.css';
+import 'fontsource-inter/700-normal.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
