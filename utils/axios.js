05a63c896e11dfdd52b3674779fe88b9c33f8f55
fcrezza <fachrezanang@gmail.com>
2021-02-10 17:22:38 +0700

    change env key

--- utils/axios.js
+++ utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080'
-    : process.env.BASE_API_URL;
+    : process.env.NEXT_PUBLIC_BASE_API_URL;
 
 const customAxiosInstance = axios.create({baseURL, withCredentials: true});
 
